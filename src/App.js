965c49b788822954eae43a743f3f9951c35de82b
snizhko <snizhkoyriy@gmail.com>
2022-02-03 12:18:30 +0200

    26,27 - relocate BLL props to index js, change /profile to root path - /

--- src/App.js
+++ src/App.js
@@ -9,34 +9,7 @@ import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 
-const App = () => {
-
-    let dialogs = [
-        {id: 1, name: 'Natasha'},
-        {id: 2, name: 'Sasha'},
-        {id: 3, name: 'Yura'},
-        {id: 4, name: 'Lesia'},
-        {id: 5, name: 'Dima'},
-        {id: 6, name: 'Dasha'}
-    ];
-
-    let messages = [
-        {id: 1, text: 'Hi!'},
-        {id: 2, text: 'How are you?'},
-        {id: 3, text: 'I\'m fine, and you?'},
-        {id: 4, text: 'Me too'},
-        {id: 5, text: 'Yo!'},
-        {id: 6, text: 'Yo Yo Yo!'},
-    ];
-
-    let posts = [
-        {id: 1, likesCount: 23, message: 'Hi!'},
-        {id: 2, likesCount: 54, message: 'It\'s my first post!'},
-        {id: 3, likesCount: 32, message: 'It\'s my first post 1!'},
-        {id: 4, likesCount: 11, message: 'It\'s my first post 2!'},
-        {id: 5, likesCount: 16, message: 'It\'s my first post 3!'},
-        {id: 6, likesCount: 9, message: 'It\'s my first post 4!'},
-    ];
+const App = (props) => {
 
     return (
         <BrowserRouter>
@@ -45,8 +18,8 @@ const App = () => {
                 <Navbar/>
                 <main className="main">
                     <Routes>
-                        <Route path="/profile" element={<Profile posts={posts} />}/>
-                        <Route path="/dialogs/*" element={<Dialogs dialogs={dialogs} messages={messages} />}/>
+                        <Route path="/" element={<Profile posts={props.posts} />}/>
+                        <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />}/>
                         <Route path="/settings" element={<Settings/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/news" element={<News/>}/>
